8245e313f456d7ae18fd984df9fa7865b77e2714
Atti Ur Rehman <atti.rehmman@gmail.com>
2024-01-07 14:11:03 +0200

    feat: Adds skills section skills.tsx component

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,10 +1,11 @@
 import Header from './components/header';
+import Skills from './components/skills';
 
 export default function Home() {
   return (
     <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
      <Header />
-
+     <Skills />
     </main>
   )
 }
