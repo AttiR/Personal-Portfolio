423a81005181216ca7e2ad037549e707735951c9
Atti Ur Rehman <atti.rehmman@gmail.com>
2024-01-07 13:08:03 +0200

    refactor: Adds separate header.js component

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,33 +1,9 @@
-import Image from 'next/image'
+import Header from './components/header';
 
 export default function Home() {
   return (
     <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
-      <section className="min-h-screen">
-      <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className="font-burtons text-xl">developedbyed</h1>
-            <ul className="flex items-center">
-
-              <li>
-                <a
-                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
-                >
-                  Skills
-                </a>
-              </li>
-            </ul>
-          </nav>
-
-      </section>
+     <Header />
 
     </main>
   )
