c32e1d5eba8a92bb118ef03c122cdc8b248e9e62
Atti Ur Rehman <atti.rehmman@gmail.com>
2024-01-09 15:28:10 +0200

    fix: The width and height of project image

--- portfolio/src/app/components/projects.tsx
+++ portfolio/src/app/components/projects.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from "next/image";
-import web from '../../../public/design.png';
+import project1 from '../../../public/project1.png';
 import data from '../../../public/data.png';
 import devops from '../../../public/Devops.png';
 
@@ -9,31 +9,18 @@ const Projects = () => {
     <section>
       <div>
         <h3 className="text-3xl py-1 dark:text-white">Projects</h3>
-        <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-            Experienced Software Engineer adept at SaaS product development using
-            <span className="text-teal-500"> Python, Dash Plotly, React, SQL, Firebase </span>
-            and <span className="text-teal-500"> Azure. </span>
-            Specialized in crafting end-to-end solutions, from planning to deployment.
-            Proficient Data Analyst in
-            <span className="text-teal-500"> Power BI </span>, transforming data into actionable insights.
-            Mobile Network RAN PoC Analyst with a knack for optimizing connectivity trials.
-            Proactive and innovative tech enthusiast.
-        </p>
-        <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-            I provide a diverse set of services, encompassing web development,
-            data analytics using Power BI, and technical consulting for effective data management and database solutions
-        </p>
+
       </div>
       <div className="lg:flex gap-10">
         <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
 
                 {/* Image takes full width of the div */}
                 <div className="w-full">
-                    <Image className="mx-auto" src={web} width={400} height={400} alt="web image" />
+                    <Image className="mx-auto" src={data} width={800} height={900} alt="web image" />
                 </div>
 
 
-                <p className="py-2">
+                <p className="py-8">
                     Crafting sophisticated web solutions tailored to your requirements, employing a foundation in core design principles.
                 </p>
 
@@ -52,10 +39,10 @@ const Projects = () => {
 
                 {/* Image takes full width of the div */}
                 <div className="w-full">
-                    <Image className="mx-auto" src={web} width={400} height={400} alt="web image" />
+                    <Image className="mx-auto" src={data} width={800} height={900} alt="web image" />
                 </div>
 
-                <p className="py-2">
+                <p className="py-8">
                     Crafting sophisticated web solutions tailored to your requirements, employing a foundation in core design principles.
                 </p>
 
@@ -74,11 +61,11 @@ const Projects = () => {
 
                 {/* Image takes full width of the div */}
                 <div className="w-full">
-                    <Image className="mx-auto" src={web} width={400} height={400} alt="web image" />
+                    <Image className="mx-auto" src={data} width={800} height={900} alt="web image" />
                 </div>
 
 
-                <p className="py-2">
+                <p className="py-8">
                     Crafting sophisticated web solutions tailored to your requirements, employing a foundation in core design principles.
                 </p>
 
