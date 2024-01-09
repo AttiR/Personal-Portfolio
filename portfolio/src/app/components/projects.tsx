1fa369255516f1764038dc0942542903e55a65b2
Atti Ur Rehman <atti.rehmman@gmail.com>
2024-01-09 15:08:46 +0200

    feat: Adjusts the div for project layout for projects section

--- portfolio/src/app/components/projects.tsx
+++ portfolio/src/app/components/projects.tsx
@@ -10,69 +10,90 @@ const Projects = () => {
       <div>
         <h3 className="text-3xl py-1 dark:text-white">Projects</h3>
         <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-        Experienced Software Engineer adept at SaaS product development using
-          <span className="text-teal-500"> Python, Dash Plotly, React, SQL, Firebase </span>
-          and <span className="text-teal-500"> Azure. </span>
-          Specialized in crafting end-to-end solutions, from planning to deployment.
-          Proficient Data Analyst in
-          <span className="text-teal-500"> Power BI </span>, transforming data into actionable insights.
-          Mobile Network RAN PoC Analyst with a knack for optimizing connectivity trials.
-          Proactive and innovative tech enthusiast.
+            Experienced Software Engineer adept at SaaS product development using
+            <span className="text-teal-500"> Python, Dash Plotly, React, SQL, Firebase </span>
+            and <span className="text-teal-500"> Azure. </span>
+            Specialized in crafting end-to-end solutions, from planning to deployment.
+            Proficient Data Analyst in
+            <span className="text-teal-500"> Power BI </span>, transforming data into actionable insights.
+            Mobile Network RAN PoC Analyst with a knack for optimizing connectivity trials.
+            Proactive and innovative tech enthusiast.
         </p>
         <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-        I provide a diverse set of services, encompassing web development,
-        data analytics using Power BI, and technical consulting for effective data management and database solutions
+            I provide a diverse set of services, encompassing web development,
+            data analytics using Power BI, and technical consulting for effective data management and database solutions
         </p>
       </div>
       <div className="lg:flex gap-10">
         <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-          <Image className="mx-auto" src={web} width={100} height={100} alt="web image"/>
-          <h3 className="text-lg font-medium pt-8 pb-2  ">
-            Full Stack Developer
-          </h3>
-          <p className="py-2">
-          Crafting sophisticated web solutions tailored to your requirements, employing a foundation in core design principles.
-          </p>
-          <h4 className="py-4 text-teal-600">Technologies I Use</h4>
-          <p className="text-gray-800 py-1">HTML</p>
-          <p className="text-gray-800 py-1">Tailwind CSS, Material UI</p>
-          <p className="text-gray-800 py-1">JavaScript, TypeScript</p>
-          <p className="text-gray-800 py-1">React, Next.js</p>
-          <p className="text-gray-800 py-1">Node, Flask, PHP</p>
-          <p className="text-gray-800 py-1">SQL, MongoDB, Azure</p>
+
+                {/* Image takes full width of the div */}
+                <div className="w-full">
+                    <Image className="mx-auto" src={web} width={400} height={400} alt="web image" />
+                </div>
+
+
+                <p className="py-2">
+                    Crafting sophisticated web solutions tailored to your requirements, employing a foundation in core design principles.
+                </p>
+
+                {/* Buttons with equal width using flex-1 */}
+                <div className="flex justify-between mt-4 gap-20">
+                    <a href="#live-preview" className="flex-1 bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">
+                        Live Preview
+                    </a>
+                    <a href="#code" className="flex-1 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded">
+                        Code
+                    </a>
+                </div>
         </div>
 
-        {/* Code Section */}
         <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-          <Image className="mx-auto" src={data} width={150} height={150} alt="data image"/>
-          <h3 className="text-lg font-medium pt-8 pb-2 ">
-            Microsoft Certified: Power BI Data Analyst
-          </h3>
-          <p className="py-2">
-          Offering comprehensive data analysis and visualization services for informed decision-making.
-          </p>
-          <h4 className="py-4 text-teal-600">Technologies I Use</h4>
-          <p className="text-gray-800 py-1">Python</p>
-          <p className="text-gray-800 py-1">SQL</p>
-          <p className="text-gray-800 py-1">Pandas, Dash, Plotly, Seaborn</p>
-          <p className="text-gray-800 py-1">Power BI</p>
-          <p className="text-gray-800 py-1">Excell</p>
+
+                {/* Image takes full width of the div */}
+                <div className="w-full">
+                    <Image className="mx-auto" src={web} width={400} height={400} alt="web image" />
+                </div>
+
+                <p className="py-2">
+                    Crafting sophisticated web solutions tailored to your requirements, employing a foundation in core design principles.
+                </p>
+
+                {/* Buttons with equal width using flex-1 */}
+                <div className="flex justify-between mt-4 gap-20">
+                    <a href="#live-preview" className="flex-1 bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">
+                        Live Preview
+                    </a>
+                    <a href="#code" className="flex-1 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded">
+                        Code
+                    </a>
+                </div>
         </div>
 
-        {/* Consulting Section */}
         <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-          <Image className="mx-auto" src={devops} width={150} height={150} alt="devops image"/>
-          <h3 className="text-lg font-medium pt-8 pb-2 ">DevOps</h3>
-          <p className="py-2">
 
-          Versatile DevOps practitioner with expertise in GitHub, CI/CD servers for automated workflows, and server-side application deployment and integration.
-          </p>
-          <h4 className="py-4 text-teal-600">Technologies I Use</h4>
-          <p className="text-gray-800 py-1">GitHub</p>
-          <p className="text-gray-800 py-1">CI/CD</p>
-          <p className="text-gray-800 py-1">Digital Ocean</p>
-          <p className="text-gray-800 py-1">Firebase</p>
+                {/* Image takes full width of the div */}
+                <div className="w-full">
+                    <Image className="mx-auto" src={web} width={400} height={400} alt="web image" />
+                </div>
+
+
+                <p className="py-2">
+                    Crafting sophisticated web solutions tailored to your requirements, employing a foundation in core design principles.
+                </p>
+
+                {/* Buttons with equal width using flex-1 */}
+                <div className="flex justify-between mt-4 gap-20">
+                    <a href="#live-preview" className="flex-1 bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">
+                        Live Preview
+                    </a>
+                    <a href="#code" className="flex-1 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded">
+                        Code
+                    </a>
+                </div>
+
         </div>
+
       </div>
     </section>
   );
